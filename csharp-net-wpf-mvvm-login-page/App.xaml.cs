f59663ad09657e171f41181f63031db4483b2760
Onur45500 <onur.akmese@viacesi.fr>
2024-10-08 14:02:23 +0200

    Initialized the project

---
+++ csharp-net-wpf-mvvm-login-page/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace csharp_net_wpf_mvvm_login_page
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
